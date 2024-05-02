4d6d214bc6e6539dc8e5ec2b1f96c5ef6f3fcf30
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-05-02 16:07:49 -0400

    trabalho dados java

---
+++ tipo-dados.js
@@ -0,0 +1,13 @@
+let nome = "Wanderson" //tipo String
+let verdadeiro = true; //tipo Boolean
+let falso = false; //tipo Boolean
+let numero = 40; //tipo Number
+let reais = 25.99; //tipo Number
+let nulo = null; //tipo Null
+let indefinido; //tipo Undefined
+let NotANumber = 0 / 0; //tipo NaN
+let objeto = {       //tipo Object
+    name: "Wanderson",
+    age: 40,
+    Profession: "Teacher"
+}
