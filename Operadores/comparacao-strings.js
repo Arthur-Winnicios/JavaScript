6f11aa40051e079ce06b48193547d9eb42b2cd23
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-05-07 15:33:30 -0400

    atividade sobre comparação de strings java

---
+++ Operadores/comparacao-strings.js
@@ -0,0 +1,8 @@
+let stringsIguais = "Wanderson" == "Wanderson"; //true
+let stringDiferentes = "wanderson" == "Wanderson"; //false
+let stringMenor = "A" < "B"; //true
+let stringMaior = "C" > "D"; //false
+console.log(`Wanderson == Wanderson: ${stringIguais}`);
+console.log(`wanderson == Wanderson: ${stringDiferentes}`);
+console.log(`A == B: ${stringMenor}`);
+console.log(`C == D: ${stringMaior}`);
