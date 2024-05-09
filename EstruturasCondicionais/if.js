270d1f417e5adef321a7afe99ce27844d96f86af
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-05-09 14:14:06 -0400

    atividade sobre estruturas condicionais IF

---
+++ EstruturasCondicionais/if.js
@@ -0,0 +1,7 @@
+let hora = 15;
+
+if (hora < 12) {
+    console.log("Bom dia!");
+}
+//O restante do código continua independentemente se a condição for verdadeira ou não
+console.log("Dinheiro não traz felicidade. Me dê o seu e seja feliz!🤪");
